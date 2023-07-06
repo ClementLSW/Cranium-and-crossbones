720166370263a87588e1448c50aea80ce7164d43
Clement Leow <clementlsw@gmail.com>
2023-07-06 15:48:27 +0800

    Inventory Done, Upgrades halfway

--- Cranium and Crossbones/Assets/JSONParser.cs
+++ Cranium and Crossbones/Assets/JSONParser.cs
@@ -12,7 +12,7 @@ public class JSONParser
 
     // Static Variable for turning on and off debugging
     // Manually change it please
-    private static bool debug = true;
+    private static bool debug = false;
 
     /// <summary>
     /// Reads a single level JSON file and returns a key value pair of strings in a 
@@ -45,6 +45,48 @@ public class JSONParser
             return null;
         }
         return result;
+    }
+
+    public class Wrapper<T>
+    {
+        public T[] Array;
+    }
+
+    public static List<Upgrades.Upgrade> ParseUpgrades(string filename) {
+        
+        Wrapper<Upgrades.Upgrade> result;
+
+        List<Upgrades.Upgrade> upgrades = new();
+
+        using (StreamReader r = new StreamReader("assets/data/" + filename + ".json"))
+        {
+            string json = r.ReadToEnd();
+            result = JsonConvert.DeserializeObject<Wrapper<Upgrades.Upgrade>>(json);
+        }
+        foreach (Upgrades.Upgrade upgrade in result.Array)
+        {
+            upgrades.Add(upgrade);
+        }
+
+        return upgrades;
+    }
+
+    public static Dictionary<string,string> ParseFromJSONString(string json)
+    {
+        Dictionary<string, string> result = new Dictionary<string, string>();
+        result = JsonConvert.DeserializeObject<Dictionary<string, string>>(json);
+        return result;
+    }
+
+    public static void WriteToFile(string filename, Dictionary<string, string> output)
+    {
+        using (StreamWriter w = new StreamWriter("assets/data/" + filename + ".json"))
+        {
+            string json = JsonConvert.SerializeObject(output);
+            w.Write(json);
+        }
+
+        return;
     }
 
 }
